672fdd9a7c27eeaee0d429e3ca38bd1b8a3fd610
Zofia Bargiela <zosiab05@gmail.com>
2024-10-11 10:16:25 +0200

    section4

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,9 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+print(f"My name is {name}.")
+age = 15
+height = 190
+print(f"I am {age} years old, and my height is {height}.")
+print(f"In 6 years I will be {age+6} years old.")
